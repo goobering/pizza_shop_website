beea1081501aed13ae592a444e36e3a6c9c63787
goobering <goobering@gmail.com>
2017-06-07 08:32:40 +0100

    Initial commit

---
+++ controller.rb
@@ -0,0 +1,93 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative './models/pizza_order'
+require_relative './models/customer'
+
+get '/pizzas' do
+  @pizzas = PizzaOrder.all()
+  erb(:index)
+end
+
+get '/pizza/new' do 
+  @customers = Customer.all()
+  erb(:new)
+end
+
+get '/pizza/:id' do
+  @pizza = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+post '/pizzas' do
+  @pizza = PizzaOrder.new(params)
+  @pizza.save()
+  erb(:create)
+end
+
+get '/pizza/:id/edit' do 
+  @pizza = PizzaOrder.find(params[:id])
+  @customers = Customer.all()
+  erb(:edit)
+end
+
+post '/pizza/:id' do
+  @pizza = PizzaOrder.find(params[:id])
+  @pizza.update(params)
+  redirect to "/pizza/#{params[:id]}"
+end
+
+post '/pizza/:id/delete' do
+  @pizza = PizzaOrder.find(params[:id])
+  @pizza.delete()
+  redirect to "/pizzas"
+end
+
+get '/customer/new' do 
+  erb(:new_customer)
+end
+
+post '/customers' do
+  @customer = Customer.new(params)
+  @customer.save()
+  erb(:create_customer)
+end
+
+get '/pizzas' do
+  @pizzas = PizzaOrder.all()
+  erb(:index)
+end
+
+get '/customers' do
+  @customers = Customer.all()
+  erb(:index_customers)
+end
+
+get '/customer/:id' do
+  @customer = Customer.find(params[:id])
+  erb(:show_customer)
+end
+
+get '/customer/:id/edit' do 
+  @customer = Customer.find(params[:id])
+  erb(:edit_customer)
+end
+
+post '/customer/:id' do
+  @customer = Customer.find(params[:id])
+  @customer.update(params)
+  redirect to "/customer/#{params[:id]}"
+end
+
+post '/customer/:id/delete' do
+  @customer = Customer.find(params[:id])
+  @customer.delete()
+  redirect to "/customers"
+end
+
+
+
+
+
+
---
+++ models/customer.rb
@@ -0,0 +1,53 @@
+require_relative  '../db/SqlRunner'
+
+class Customer
+
+  attr_accessor :name, :id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+  end
+
+  def self.all()
+    sql =  "SELECT * FROM customers;"
+    customers = SqlRunner.run(sql)
+    return customers.map { |options| Customer.new(options)}
+  end
+
+  def save()    
+    sql = "INSERT INTO customers (name) VALUES ('#{@name}') returning * ;"
+   result = SqlRunner.run(sql)
+   @id = result[0]['id'].to_i()
+  end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE customers.id = #{@id}"
+    SqlRunner.run(sql)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers;"
+    SqlRunner.run(sql)
+  end
+
+  def update(options)
+    sql = "UPDATE customers SET name = '#{options['name']}' WHERE id = #{options['id']};"
+    SqlRunner.run(sql)
+  end
+
+  def orders()
+    sql = "SELECT * FROM pizza_orders WHERE customer_id = #{@id}"
+    result = SqlRunner.run(sql)
+    orders = result.map { |order| PizzaOrder.new(order)}
+    return orders
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM customers WHERE id=#{id};"
+    pizza = SqlRunner.run( sql )
+    result = Customer.new( pizza.first )
+    return result
+  end
+
+end
